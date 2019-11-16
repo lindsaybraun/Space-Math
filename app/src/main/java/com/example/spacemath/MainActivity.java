4cc2143a9cf6c17ede39b15811cc3069923ca6c2
audreypatricia <55723938+audreypatricia@users.noreply.github.com>
2019-11-16 14:46:39 +1100

    created Fragment with all lessons (addition, subtraction, multiplication, division)

--- app/src/main/java/com/example/spacemath/MainActivity.java
+++ app/src/main/java/com/example/spacemath/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.spacemath;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -7,18 +8,49 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     //TODO: create other fragment for learning activity and related files
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Fragment fragment = new GameHomeFragment();
-        swapFragment(fragment);
+        bottomNavigationView = findViewById(R.id.nav_view);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                if (menuItem.getItemId() == R.id.nav_learn) {
+
+                    Fragment fragment = new LearnHomeFragment();
+                    swapFragment(fragment);
+
+
+                    return true;
+                } else if (menuItem.getItemId() == R.id.nav_funFact) {
+
+
+                    return true;
+                } else if (menuItem.getItemId() == R.id.nav_game) {
+                    Fragment fragment = new GameHomeFragment();
+                    swapFragment(fragment);
+
+
+                    return true;
+                }
+                return false;
+            }
+        });
+
+
 
 
 
