e679ab04df7d69b4c1a2bb21da7e8c9eab6a405e
lindsaybraun <lindsay@mypicante.com>
2019-11-20 16:02:19 +1100

    added Space Image api set up

---
+++ app/src/main/java/com/example/spacemath/Links.java
@@ -0,0 +1,15 @@
+package com.example.spacemath;
+
+import java.util.List;
+
+public class Links {
+    String href;
+
+    public Links(String href){
+        this.href = href;
+    }
+
+    public String getHref() {
+        return href;
+    }
+}
