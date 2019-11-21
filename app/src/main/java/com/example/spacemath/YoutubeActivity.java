cc7afd5d538f1b8c79427331adf9ee20c774132a
audreypatricia <55723938+audreypatricia@users.noreply.github.com>
2019-11-21 22:54:39 +1100

    youtube videos fixed to run without stopping

--- app/src/main/java/com/example/spacemath/YoutubeActivity.java
+++ app/src/main/java/com/example/spacemath/YoutubeActivity.java
@@ -2,53 +2,29 @@ package com.example.spacemath;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
 
-public class TopicDetailActivity extends YouTubeBaseActivity {
+public class YoutubeActivity extends YouTubeBaseActivity {
 
-    public ImageView topicImage;
-    public TextView topicTitle;
-    public TextView topicIntro;
-    public TextView topicContent;
-    public TextView topicSummary;
     private Button play;
     private YouTubePlayerView youTubePlayerView;
     private YouTubePlayer.OnInitializedListener onInitializedListener;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_topic_detail);
+        setContentView(R.layout.activity_youtube);
 
         Intent intent = getIntent();
-        final Topic currentTopic = (Topic) intent.getSerializableExtra("Topic");
-
-        topicTitle = findViewById(R.id.tv_topicTitle);
-        topicIntro = findViewById(R.id.tv_topicIntro);
-        topicContent = findViewById(R.id.tv_topicContent);
-        topicSummary = findViewById(R.id.tv_topicSummary);
-        topicImage = findViewById(R.id.topicImageDetail);
-
-        topicTitle.setText(currentTopic.getTopicName());
-        topicIntro.setText(currentTopic.getTopicIntro());
-        topicContent.setText(currentTopic.getTopicContent());
-        topicSummary.setText(currentTopic.getTopicSummary());
-        topicImage.setImageResource(currentTopic.getTopicImage());
-
-
+        final Topic currentTopic = (Topic) intent.getSerializableExtra("YoutubeVideo");
 
 
         youTubePlayerView = (YouTubePlayerView)findViewById(R.id.youtubeVid);
@@ -67,12 +43,8 @@ public class TopicDetailActivity extends YouTubeBaseActivity {
         play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                youTubePlayerView.initialize("AIzaSyBEhglmux-UV8N73ohEr-x4ziSFRrAYx38 ",onInitializedListener);
-                Intent i = new Intent(getApplicationContext(), YoutubeActivity.class);
-                i.putExtra("YoutubeVideo", currentTopic);
-                getApplicationContext().startActivity(i);
+                youTubePlayerView.initialize("AIzaSyBEhglmux-UV8N73ohEr-x4ziSFRrAYx38 ",onInitializedListener);
             }
         });
-
     }
 }
