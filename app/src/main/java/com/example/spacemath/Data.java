2430930b6c7117902bdbbb102df23149ef5f3d47
lindsaybraun <lindsay@mypicante.com>
2019-11-21 12:50:13 +1100

    added Space Image api feature

--- app/src/main/java/com/example/spacemath/Data.java
+++ app/src/main/java/com/example/spacemath/Data.java
@@ -1,13 +1,13 @@
 package com.example.spacemath;
 
 public class Data {
-    String date_created;
+    String title;
 
-    public Data(String date_created){
-        this.date_created = date_created;
+    public Data(String title){
+        this.title = title;
     }
 
-    public String getDate_created() {
-        return date_created;
+    public String getTitle() {
+        return title;
     }
 }
--- app/src/main/java/com/example/spacemath/SpaceImage.java
+++ app/src/main/java/com/example/spacemath/SpaceImage.java
@@ -3,21 +3,19 @@ package com.example.spacemath;
 public class SpaceImage {
     //items grouping and then each object has a links grouping and a data grouping
 
-    String collection;
-    Item[] items;
+    Collection collection;
 
 
-    public SpaceImage(String collection, Item[] items){
+
+    public SpaceImage(Collection collections){
         this.collection = collection;
-        this.items = items;
 
     }
 
     //get image url
-    public String getCollection() {
+    public Collection getCollection() {
         return collection;
     }
-    public Item[] getItems() { return items;}
 
 
 }
